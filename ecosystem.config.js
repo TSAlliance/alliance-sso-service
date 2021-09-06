65355fcb9968c838bfa66e9835fac10bd6fc3742
Zitzmann <cedric.zitzmann@gmail.com>
2021-09-06 17:21:43 +0200

    added deployment settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,29 @@
 module.exports = {
     apps: [{
         name: "Alliance-SSO",
-        script: "./dist/main.js",
-        exec_interpreter: "node",
+        script: "npm",
+        args: "run start:dev",
         watch: false,
         increment_var: "APP_PORT",
+        instances: 1,
+        time: false,
         env: {
-            NODE_ENV: "production",
-            APP_PORT: 3001
+            APP_PORT: 3388
         }
-    }]
+    }],
+    deploy: {
+        production: {
+            user: 'deploy.name',
+            host: 'tsalliance.eu',
+            key: 'deploy.key',
+            ref: 'origin/main',
+            repo: 'https://github.com/username/myapp',
+            path: '/home/username/myapp',
+            'post-deploy':
+                'npm install && npm run build && pm2 reload ecosystem.config.js --env production && pm2 save && git checkout yarn.lock',
+            env: {
+                NODE_ENV: 'production',
+            }
+        }
+    }
 }
